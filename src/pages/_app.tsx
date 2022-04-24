386bed33238363ba0e3de073bafb45405e0ec8f0
Matheus Matavelli <matheusmatavellii@gmail.com>
2022-04-24 18:24:41 -0300

    initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import '../styles/globals.scss';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
